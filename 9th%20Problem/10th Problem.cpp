e32e549bb96e5bbe539fc65c8889544ca5f02b4e
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-07-05 15:21:45 +0300

    Create 10th Problem.cpp

---
+++ 9th%20Problem/10th Problem.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+    #include<iomanip>
+    int main()
+    {
+        char firstSymbol, secondSymbol, thirdSymbol;
+        std::cin >> firstSymbol >> secondSymbol >> thirdSymbol;
+
+        int firstNum = firstSymbol - '0';
+        int secondNum = secondSymbol - '0';
+        int thirdNum = thirdSymbol - '0';
+
+        int result = firstNum * 100 + secondNum * 10 + thirdNum;
+        std::cout << result << std::endl;
+
+        return 0;
+
+       
+    }
