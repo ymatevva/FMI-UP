c90639b08a1026096804f9140077b03a39724b6d
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-07-09 00:27:15 +0300

    Create 10th Problem.cpp

---
+++ 10th Problem.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<cassert>
+//checking what kind of char we have typed
+
+int main()
+{
+    char symbol ;
+    std::cin >> symbol;
+
+    int symbolChanged = (int)symbol;
+
+    if (symbol >= 'A' && symbol <= 'Z')
+    {
+        std::cout << symbol << std::endl;
+    }
+    else if (symbol >= 'a' && symbol <= 'z')
+    {
+        std::cout << char(symbol - 'a' + 'A') << std::endl;
+    }
+    else
+    {
+        std :: cout << "Invalid input!" << std :: endl;
+    }
+  return 0;
+}
