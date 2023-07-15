307f649e87fc03d8243fb23e62627a1bd7312298
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-07-16 00:23:41 +0300

    Create 8th Problem.cpp

---
+++ 8th Problem.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+// finding the prime divisors
+int main()
+{
+    int n;
+    std::cin >> n;
+  
+    int k = 2;
+  
+    while (n != 1)
+    {
+        if (n % k == 0)
+        {
+            std::cout << k << " ";
+            while (n % k == 0)
+            {
+                n /= k;
+            }
+        }
+        k++;
+    }
+}
