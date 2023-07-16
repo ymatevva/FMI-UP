bd054f80ba64819e86555f182309c77a5c6a0799
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-07-16 18:16:26 +0300

    Create 15th Problem.cpp

---
+++ 15th Problem.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<cmath>
+// checking if the sum of the divisors of the numbers is equal to the original num
+int main()
+{
+  int num1, num2;
+  std :: cin >> num1 >> num2;
+  int max, min;
+
+  if(num1 > num2)
+  { max = num1; min = num2;}
+  else
+  { max = num2; min = num1;}
+
+  for (int i = min ; i <= max; i++)
+    {
+        int number = i;
+        int sum = 0;
+
+        for (int j = 1; j < number ; j++)
+        {
+                if (number % j == 0)
+                {
+                    sum += j;
+                }
+        }
+
+        if(sum==i)
+        {
+            std :: cout << i  << " is a perfect number " << std :: endl;
+        }
+    }
+    return 0;
+}
