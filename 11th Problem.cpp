f122e602959e1469662e72b5be51f00305b2a925
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-07-16 12:21:03 +0300

    Create 11th Problem.cpp

---
+++ 11th Problem.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+//finding the reverse num and finding if it is a palindorme
+int main()
+{
+    int n;
+    std :: cin >> n;
+    int originalNum = n;
+    int reverse = 0;
+    int remainder;
+    while(n > 0)
+    {
+        remainder = n % 10;
+        reverse = reverse*10 + remainder;
+        n/=10;
+
+    }
+
+    std :: cout << reverse << std :: endl;
+
+     if( originalNum == reverse)
+     {
+         std::cout << " palindrome" << std::endl;
+     }
+     else if (originalNum != reverse || n < 0)
+     {
+         std :: cout << "not palindrome" << std :: endl;
+     }
+
+    return 0;
+}
