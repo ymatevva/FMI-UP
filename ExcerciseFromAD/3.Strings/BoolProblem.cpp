db8172acbb3861a32e9540d36efa3930135aa31f
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-09-09 23:35:01 +0300

    Add files via upload

---
+++ ExcerciseFromAD/3.Strings/BoolProblem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+bool isTrue(int number)
+{
+    int lastLastDigit = (number/10)%10;
+    if (lastLastDigit==0||number%lastLastDigit!=0)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isInPainted(double x,double y)
+{
+    if (x>=-2 && x<=1 && )
+}
+
+int main()
+{
+    int num;
+    std :: cin >> num;
+
+    double x,y;
+    std :: cin >> x >> y;
+
+
+
+
+
+
+
+
+    return 0;
+}
