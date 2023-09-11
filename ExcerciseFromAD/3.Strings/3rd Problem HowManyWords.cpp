5435b536720cf70e54b06a05a579b0379505df96
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-09-11 21:15:38 +0300

    Add files via upload

---
+++ ExcerciseFromAD/3.Strings/3rd Problem HowManyWords.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstring>
+const int MAX_SIZE = 70;
+
+int numWords(char *str)
+{
+    int count = 0;
+    bool isTExt = false;
+
+    for (int i = 0; i < strlen(str); ++i)
+    {
+        if (isalpha(str[i]))
+        {
+            isTExt = true;
+        }
+        if (isalpha(str[i]) && str[i+1]=='\0')
+        {
+            count++;
+        }
+
+        else if (str[i] == ' ')
+        {
+            if (isTExt)
+            {
+                count++;
+                isTExt = false;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    return count;
+}
+int main()
+{
+    char str[MAX_SIZE];
+    std::cin.getline(str,MAX_SIZE);
+
+    std :: cout << numWords(str) << std :: endl;
+
+    return 0;
+}
