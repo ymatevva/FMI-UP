fefc10e287bd71d13e96910e6e1a044f0e3a3613
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-10-10 13:04:54 +0300

    Add files via upload

---
+++ IS/ADPracticumsSeminars/Sem03/8thProblem.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cassert>
+//time after 15 minutes
+
+int main()
+{
+	int hour, minutes;
+	std::cin >> hour >> minutes;
+	assert(hour >= 0 && hour <= 23);
+	assert(minutes >= 0 && minutes <= 59);
+
+	int newMinutes = minutes += 15;
+	int newHour = hour += newMinutes / 60;
+	newMinutes = minutes % 60;
+
+	if (newHour >= 24)
+	{
+		newHour = newHour % 24;
+	}
+
+	if (newHour <= 9)
+	{
+		std::cout << "0" << newHour;
+	}
+	else
+	{
+		std::cout << newHour;
+	}
+
+	std::cout << " : ";
+
+	if (newMinutes <= 9)
+	{
+		std::cout << "0" << newMinutes;
+	}
+	else
+	{
+		std::cout << newMinutes;
+	}
+	
+
+
+
+	return 0;
+}
