88327ba37ed690c6b191914dc5757c926504f0c4
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-10-12 10:59:12 +0300

    Add files via upload

---
+++ IS/ADPracticumsSeminars/Sem03/8thProblemSem3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+int main()
+{
+
+	int n;
+	std::cin >> n;
+
+	for (size_t i = 1; i <= n; i++)
+	{
+		for (size_t j = 1; j <= i; j++)
+		{
+			std::cout << "$" << " ";
+		}
+		std::cout << std::endl;
+	}
+
+
+
+
+
+
+	return 0;
+}
