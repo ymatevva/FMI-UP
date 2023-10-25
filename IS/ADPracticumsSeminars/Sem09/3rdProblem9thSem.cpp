4df8decf3aa67c266a8af1bf2fa34792aba9feea
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-10-25 13:58:57 +0300

    Add files via upload

--- IS/ADPracticumsSeminars/Sem09/3rdProblem9thSem.cpp
+++ IS/ADPracticumsSeminars/Sem09/3rdProblem9thSem.cpp
@@ -1,66 +1,56 @@
 #include<iostream>
 const int MAX_SIZE = 60;
-void transpose(int matrix1[][MAX_SIZE], int row1, int col1, int matrix2[][MAX_SIZE], int row2, int col2, int result[][MAX_SIZE])
+void swap(int&a, int& b)
 {
-	if (col1 != row2)
-		return;
-
-	for (size_t i = 0; i < row1; i++)
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void sortInFirst(int array1[], int array2[], int& n1, int& n2)
+{
+	int start = n1 ;
+    n1 = start + n2;//the number of elements in the second array
+	for (size_t i = start, j = 0; i < n1; i++,j++)
 	{
-		for (size_t j = 0; j < col2; j++)
+		array1[i] = array2[j];
+	}
+
+	for (size_t i = 0; i < n1; i++)
+	{
+		for (size_t j = i+1; j < n1 ; j++)
 		{
-			for (size_t k= 0; k < col1; k++)
-			{
-				result[i][j] = matrix1[i][k] * matrix2[k][j];
-			}
+			if (array1[i] > array1[j])
+				swap(array1[i], array1[j]);
 		}
 	}
+
 }
 int main()
-{
-	int row1, col1;
-	std::cin >> row1 >> col1;
-
-	int matrix1[MAX_SIZE][MAX_SIZE];
-
-	for (int i = 0; i < row1; i++)
+{   
+	int n1;
+	std::cin >> n1;
+	int array1[MAX_SIZE];
+	for (size_t i = 0; i < n1;i++)
 	{
-		for (size_t j = 0; j < col1; j++)
-		{
-          std::cin >> matrix1[i][j];
-		}	
+		std::cin >> array1[i];
 	}
 
-	int row2, col2;
-	std::cin >> row2 >> col2;
 
-	int matrix2[MAX_SIZE][MAX_SIZE];
-
-	for (int i = 0; i < row2; i++)
+	int n2;
+	std::cin >> n2;
+	int array2[MAX_SIZE];
+	for (size_t i = 0; i < n2; i++)
 	{
-		for (size_t j = 0; j < col2; j++)
-		{
-			std::cin >> matrix2[i][j];
-		}
+		std::cin >> array2[i];
+   	}
+
+	sortInFirst(array1, array2, n1, n2);
+	for (size_t i = 0; i < n1 ; i++)
+	{
+		std::cout << array1[i] << " ";
 	}
 
-	if (col1 != row2)
-	{
-		std::cout << "The matrixes are not compatible" << std::endl;
-		return 0;
-	}
 
-	int result[MAX_SIZE][MAX_SIZE];
-
-	transpose(matrix1, row1, col1, matrix2, row2, col2, result);
-	for (size_t i = 0; i < row1; i++)
-	{
-		for (size_t j = 0; j < col2; j++)
-		{
-			std::cout << result[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
 
 	return 0;
 }
