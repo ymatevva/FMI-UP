710187d1e1d92345ffd44ee4a92fb25f80fb9063
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2024-01-07 19:15:29 +0200

    Add files via upload

--- IS/Exam1PrepPracticum/5thProblem.cpp
+++ IS/Exam1PrepPracticum/5thProblem.cpp
@@ -1,43 +1,96 @@
 #include<iostream>
+constexpr int MAX_SIZE = 100;
 
-unsigned int digitsMultiplied(unsigned int n)
-{ 
-	if (n == 0)
-		return 0;
-
-	unsigned int res = 1;
-	while (n != 0)
-	{
-		res *= n % 10;
-		n /= 10;
-	}
-	return res;
-}
-
-unsigned int countNumsWithProductDigitsOneDigit(const int arr[], size_t size)
+unsigned int countSymbolOcc(const char* str, char token)
 {
+	if (!str)
+		return 0;
 	unsigned int count = 0;
-	for (size_t i = 0; i < size; i++)
+	while (*str)
 	{
-		if (digitsMultiplied(arr[i]) >= 0 && digitsMultiplied(arr[i]) <= 9)
+		if (*str == token)
 			count++;
+		str++;
 	}
 	return count;
 }
+
+int getTokenPos(const char* str, char token)
+{
+	if (!str)
+		return 0;
+	for (size_t i = 0; ; i++)
+	{
+		if (str[i] == token || str[i] == '\0')
+			return i;
+	}
+	return -1;
+}
+
+void copyNChars(const char* source, char* dest, int N)
+{
+	if (!source || !dest)
+		return;
+	for (size_t i = 0; i < N; i++)
+		dest[i] = source[i];
+}
+
+char** splitString(const char* str, char token)
+{
+	if (!str)
+		return nullptr;
+
+	size_t resSize = countSymbolOcc(str, token) + 2;
+	unsigned int resInd = 0;
+	char** result = new char* [resSize];
+	result[resSize - 1] = nullptr;
+
+	while (*str)
+	{
+		int tokPos = getTokenPos(str, token);
+		char* word = new char[tokPos + 1];
+		word[tokPos] = '\0';
+		copyNChars(str, word, tokPos);
+
+		result[resInd++] = word;
+		str += tokPos;
+
+		if (*str == token)
+			str++;
+	}
+	return result;
+}
+
+void printSplit(char** str)
+{
+	while (*str)
+	{
+		std::cout << *str << std::endl;
+		str++;
+	}
+}
+
+void freeSplit(char** str)
+{
+	if (!str)
+		return;
+	for (size_t i = 0; str[i] != nullptr; i++)
+	{
+		delete[] str[i];
+	}
+	delete[] str;
+}
 int main()
 {
-	constexpr int MAX_SIZE = 1024;
+	char str[MAX_SIZE];
+	std::cin.getline(str, MAX_SIZE);
 
-	unsigned int n;
-	std::cin >> n;
+	char symbol;
+	std::cin >> symbol;
 
-	int arr[MAX_SIZE];
+	char** result = splitString(str, symbol);
 
-	for (size_t i = 0; i < n; i++)
-	{
-		std::cin >> arr[i];
-	}
-
-	std::cout << countNumsWithProductDigitsOneDigit(arr, n);
+	printSplit(result);
+	freeSplit(result);
 	return 0;
 }
